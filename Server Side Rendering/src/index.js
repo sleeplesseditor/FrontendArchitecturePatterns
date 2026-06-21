36ba7b2ad5c0d74460f0e87aadac15c4df26fd5e
sleeplesseditor <josh@bryson.me.uk>
2026-06-22 00:26:29 +0100

    SSR solution variant applied

---
+++ Server Side Rendering/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", async (req, res) => {
+    const pikachuRequest = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/pikachu"
+    );
+    const pikachu = await pikachuRequest.json();
+
+    res.render("index", {
+        message: `Hello, Server-Side Rendering! You are seeing ${pikachu.name}`,
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
